fc0e550a717f040a83ac87c263a8b1c8e419474a
Brenton MOODLEY <bmoodley@e5r6p8.jnb.42.fr>
2017-07-01 20:33:21 +0200

    ft_lstadd_back.c update

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,22 +6,19 @@
 /*   By: bmoodley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/01 19:41:57 by bmoodley          #+#    #+#             */
-/*   Updated: 2017/07/01 20:15:20 by bmoodley         ###   ########.fr       */
+/*   Updated: 2017/07/01 20:32:55 by bmoodley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_lstadd_back(t_list **alst, void *content)
 {
-	t_list		*current;
-
-	current = *alst;
-	if (current)
+	if (*alst)
 	{
-		while (current->next)
+		while ((*alst)->next)
 		{
-			current = current->next;
+			*alst  = (*alst)->next;
 		}
-		current->next = ft_lstnew(content, 1)
+		(*alst)->next = ft_lstnew(content, 1)
 	}
 	else
 		*alst = ft_lstnew(content, 1);
