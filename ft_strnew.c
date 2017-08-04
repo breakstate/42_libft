01b7426a8863bd7f8cd3757d9f747a907bb78725
Brenton MOODLEY <bmoodley@e4r16p11.wtc.co.za>
2017-08-04 11:32:08 +0200

    removed +1 fpr null terminator

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: bmoodley <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 10:25:14 by bmoodley          #+#    #+#             */
-/*   Updated: 2017/06/10 16:33:44 by bmoodley         ###   ########.fr       */
+/*   Updated: 2017/08/04 11:30:49 by bmoodley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strnew(size_t size)
 {
 	char	*new;
 
-	new = (char *)malloc(sizeof(char) * size + 1);
+	new = (char *)malloc(sizeof(char) * size);
 	if (new == NULL)
 		return (NULL);
-	ft_bzero(new, size + 1);
+	ft_bzero(new, size);
 	return (new);
 }
