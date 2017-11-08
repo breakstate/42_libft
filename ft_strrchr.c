330d0377b258b8ec8fd4e88c5373f8505670cf6f
Brenton Moodley <bmoodley@student.42.fr>
2017-11-08 20:46:44 +0200

    fixed ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -17,11 +17,9 @@ char	*ft_strrchr(const char *s, int c)
 	size_t s_len;
 
 	s_len = ft_strlen(s);
-	while (s_len > 0)
-	{
-		if (s[s_len] == (char)c)
-			return ((char *)(&s[s_len]));
+	while (s_len && s[s_len] != (char)c)
 		s_len--;
-	}
+	if (s[s_len] == (char)c)
+		return ((char *)(&s[s_len]));
 	return (NULL);
 }
